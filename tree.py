06785e720df7e70a7621d920d41095b864d9bd1d
Patrick Sullivan <16sullivanp@gmail.com>
2019-09-17 14:45:00 -0400

    New File

---
+++ tree.py
@@ -0,0 +1,10 @@
+>>> def main():
+	h= int(input("Enter height of tree:"))
+	if h < 0:
+		print("The tree height is invalid. Try again!")
+	for i in range (h):
+		print(" "*(h-i-1)+"#"*(2*i+1))
+		print(" "*(h-1)+"#")
+
+		
+>>> main()
